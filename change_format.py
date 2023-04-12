69adad8e5a7d81d9ef2f05bb9362b237c0889ee8
TAWSIF AHMED <stanfordblaze4@gmail.com>
2023-04-13 01:51:05 +0600

    Delete change_format.py

--- change_format.py
+++
@@ -1,7 +0,0 @@
-from pydub import AudioSegment
-
-# Load AAC file
-audio = AudioSegment.from_file('audio_noise.aac', format='aac')
-
-# Export as WAV file
-audio.export('audio_file.wav', format='wav')
